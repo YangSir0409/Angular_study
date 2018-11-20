a1eb25ff575b623978a589efb6d45af497a73e74
YangSir0409 <542670346@qq.com>
2018-11-20 17:43:11 +0800

    11.20company

--- build/js/index.js
+++ build/js/index.js
@@ -17,46 +17,13 @@ angular.module('app').config(['$stateProvider','$urlRouterProvider',function($st
 		url:'/company/:id',
 		templateUrl:'view/company.html',
 		controller:'companyCtrl'
+	}).state('search',{
+		url:'/search',
+		templateUrl:'view/search.html',
+		controller:'searchCtrl'
 	});
 	$urlRouterProvider.otherwise('main')
 }])
-
-'use strict';
-angular.module('app').controller('companyCtrl',['$http','$state','$scope',function($http,$state,$scope){
-	$http.get("/data/company.json?id=" + $state.params.id).success(function(resp){
-		$scope.company = resp;
-	})
-}]);
-
-'use strict';
-angular.module('app').controller('mainCtrl',['$http','$scope',function($http,$scope){
-	$http.get('/data/positionList.json').success(function(resp){
-		$scope.list = resp;
-	});
-}]);
-
-'use strict';
-angular.module('app').controller('positionCtrl',['$q','$http','$state','$scope',function($q,$http,$state,$scope){
-	$scope.isLogin = false;
-	function getPosition(){
-		var def = $q.defer();
-		$http.get("/data/position.json?id=" + $state.params.id).success(function(resp){
-			$scope.position = resp;
-			def.resolve(resp);
-		}).error(function(err){
-			def.reject(err);
-		});
-		return def.promise;
-	}
-	function getCompany(id){
-		$http.get('/data/company.json?id=' + id).success(function(resp){
-			$scope.company = resp;
-		})
-	}
-	getPosition().then(function(obj){
-		getCompany(obj.companyId);
-	});
-}]);
 
 'use strict';
 angular.module('app').directive('appCompany',[function(){
@@ -160,3 +127,56 @@ angular.module('app').directive('appPositionList',[function(){
 		}
 	}
 }])
+
+'use strict';
+angular.module('app').directive('appTab',[function(){
+	return {
+		restrict: 'A',
+		replace: true,
+		templateUrl: 'view/template/tab.html'
+	}
+}])
+
+'use strict';
+angular.module('app').controller('companyCtrl',['$http','$state','$scope',function($http,$state,$scope){
+	$http.get("/data/company.json?id=" + $state.params.id).success(function(resp){
+		$scope.company = resp;
+	})
+}]);
+
+'use strict';
+angular.module('app').controller('mainCtrl',['$http','$scope',function($http,$scope){
+	$http.get('/data/positionList.json').success(function(resp){
+		$scope.list = resp;
+	});
+}]);
+
+'use strict';
+angular.module('app').controller('positionCtrl',['$q','$http','$state','$scope',function($q,$http,$state,$scope){
+	$scope.isLogin = false;
+	function getPosition(){
+		var def = $q.defer();
+		$http.get("/data/position.json?id=" + $state.params.id).success(function(resp){
+			$scope.position = resp;
+			def.resolve(resp);
+		}).error(function(err){
+			def.reject(err);
+		});
+		return def.promise;
+	}
+	function getCompany(id){
+		$http.get('/data/company.json?id=' + id).success(function(resp){
+			$scope.company = resp;
+		})
+	}
+	getPosition().then(function(obj){
+		getCompany(obj.companyId);
+	});
+}]);
+
+'use strict';
+angular.module('app').controller('searchCtrl',['$http','$scope',function($http,$scope){
+	$http.get('data/positionList.json').success(function(resp){
+		$scope.positionList = resp;
+	});
+}]);
